6ebd83769397c832d364634a2026811d568c3030
NightSiesta <qwer3582919.360354542@gmail.com>
2021-04-22 22:02:36 +0800

    fix the type error

--- cmd2func/pigCmd2func.js
+++ cmd2func/pigCmd2func.js
@@ -8,7 +8,7 @@ const welcomeMessageConsumerClass = require('../consumers/welcomeMessageConsumer
 const blessConsumerClass = require('../consumers/blessConsumer');
 const randomConsumerClass = require('../consumers/randomConsumer');
 let consumerList = [welcomeMessageConsumerClass,versionKingsConsumerClass,seaLineConsumerClass,raiderConsumerClass,helpConsumerClass,fourKingsConsumerClass,cdConsumerClass,blessConsumerClass,randomConsumerClass];
-const parameter = "cat";
+const parameter = "pig";
 function cmd2func(ctx){
     var message = [];
     var log = [];
