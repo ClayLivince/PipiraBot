56baf189607aa602c7d5d68bf65ad37ad79955fa
NightSiesta <qwer3582919.360354542@gmail.com>
2021-04-23 09:25:41 +0800

    change the welcome message and add some message to the sealine

--- consumers/welcomeMessageConsumer.js
+++ consumers/welcomeMessageConsumer.js
@@ -9,7 +9,7 @@ class welconeConsumerClass extends Consumer{
         return ctx.request.body.notice_type &&  ctx.request.body.notice_type == 'group_increase';
     }
     work(){
-        this.message[0] = welcomeMessages[this.serverName];
+        this.message[0] = `[CQ:at,qq=${this.ctx.request.body.user_id}]`+welcomeMessages[this.serverName];
         this.log = {
             "group":this.ctx.request.body.group_id,
             "type":"welocome",
