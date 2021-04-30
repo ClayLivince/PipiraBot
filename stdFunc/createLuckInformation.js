eaedff3c959b08510547cb15bcd5d0ae5e3ee747
NightSiesta <qwer3582919.360354542@gmail.com>
2021-04-30 19:45:45 +0800

    fix luck texts

--- stdFunc/createLuckInformation.js
+++ stdFunc/createLuckInformation.js
@@ -113,12 +113,7 @@ function createLuckInfomation(user_id){
         luckyPlace = luckyPlacesList[placeIndex];
         luckyPet = luckyPetsList[petIndex];
         luckyPoem = poemList[poemIndex];
-        messages = `[CQ:at,qq=${user_id}]的今日运势为：${luck}——${luckName}\n
-        幸运宠物：${luckyPet}\n
-        幸运料理：${luckyFood}\n
-        ${luckyPlace}\n
-        你仿佛听到皮皮拉鱼神在远处低语:"${luckyPoem}"
-        `
+        messages = `[CQ:at,qq=${user_id}]的今日运势为：${luck}——${luckName}\n幸运宠物：${luckyPet}\n幸运料理：${luckyFood}\n十二神守护之地：${luckyPlace}\n你仿佛听到皮皮拉鱼神在远处低语:"${luckyPoem}"`
         ban.banList.push(user_id);
         return{messages,luck,luckyFood,luckyPet,luckyPlace,luckyPoem}
     }
