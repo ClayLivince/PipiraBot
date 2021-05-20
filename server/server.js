0bae0b324c9fe1c95563872af6ebfef65597594f
NightSiesta <qwer3582919.360354542@gmail.com>
2021-05-20 09:53:56 +0800

    1.Fix the stupid error

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ app.use(async ctx =>{
     if(!nameAndConsumers){nameAndConsumers = groupServerConsumers[88888888];} //Default
     for(let index = 0; index<nameAndConsumers['consumers'].length;index++){
         if(nameAndConsumers['consumers'][index].valid(ctx)){
-            var consumer = new nameAndConsumers['consumers'](ctx,nameAndConsumers['name']);
+            var consumer = new nameAndConsumers['consumers'][index](ctx,nameAndConsumers['name']);
             consumer.work(); //工作
         }
     }
