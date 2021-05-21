e588a345ba4bfbefb4c9dcd8b520a3f9f048bc1a
NightSiesta <qwer3582919.360354542@gmail.com>
2021-05-21 23:08:41 +0800

    1.Fix the price consumer and make it offline part

--- consumers/priceConsumer.js
+++ consumers/priceConsumer.js
@@ -1,4 +1,6 @@
 const Consumer = require("./Consumer");
+const ItemJson = require('../resources/item.json');
+ItemJson = JSON.parse(ItemJson);
 const sendGroupMessage = require('../stdFunc/sendGroupMessage');
 const axios = require("axios");
 class priceConsumerClass extends Consumer{
@@ -21,41 +23,39 @@ class priceConsumerClass extends Consumer{
             else{
                 //先处理商品名
                 var item = dealtMessage[0];
-                var itemUrl = `https://cafemaker.wakingsands.com/search?string=${item}`;
-                itemUrl = encodeURI(itemUrl);
-                axios.get(itemUrl).then((res)=>{
-                    console.log("itemGotten!");
-                    if(res.data.Results!=[]){ //查询的物品对
-                        var itemID = res.data.Results[0].ID;
-                        var server = dealtMessage[1];
-                        if(server == "鸟"){
-                            server = "陆行鸟";
-                        }
-                        else if(server == "猪"){
-                            server = "莫古力";
-                        }
-                        else if(server == "猫"){
-                            server = "猫小胖";
-                        }
-                        var priceUrl = `https://universalis.app/api/${server}/${itemID}`;
-                        priceUrl = encodeURI(priceUrl);
-                        axios.get(priceUrl).then((res)=>{
-                            console.log("priceGotten!")
-                            if(res.data.listings != []){
-                                var updateDate = res.data.lastUploadTime;
-                                var data = res.data.listings;
-                                var priceMessage = `${dealtMessage[0]} ${server}的价格是：\n`;
-                                var count = (data.length<=5?data.length:5);
-                                for(let i = 0; i<count; i++){
-                                    priceMessage+=(`${data[i].pricePerUnit}*${data[i].quantity} ${(data[i].hq?"HQ":'')} 服务器：${data[i].worldName}\n`)
-                                }
-                                priceMessage+=`数据更新时间：${new Date(updateDate).getFullYear()}年${new Date(updateDate).getMonth()+1}月${new Date(updateDate).getDate()}日 ${new Date(updateDate).getHours()}时${new Date(updateDate).getMinutes()}分`;
-                                console.log(priceMessage)
-                                sendGroupMessage(this.port,this.ctx.request.body.group_id,priceMessage)
+                if(server == "鸟"){
+                    server = "陆行鸟";
+                }
+                else if(server == "猪"){
+                    server = "莫古力";
+                }
+                else if(server == "猫"){
+                    server = "猫小胖";
+                }
+                var itemID = ItemJson[item];
+                if(key){
+                    var priceUrl = `https://universalis.app/api/${server}/${itemID}`;
+                    priceUrl = encodeURI(priceUrl);
+                    axios.get(priceUrl).then((res)=>{
+                        console.log("priceGotten!")
+                        if(res.data.listings != []){
+                            var updateDate = res.data.lastUploadTime;
+                            var data = res.data.listings;
+                            var priceMessage = `${dealtMessage[0]} ${server}的价格是：\n`;
+                            var count = (data.length<=5?data.length:5);
+                            for(let i = 0; i<count; i++){
+                                priceMessage+=(`${data[i].pricePerUnit}*${data[i].quantity} ${(data[i].hq?"HQ":'')} 服务器：${data[i].worldName}\n`)
                             }
-                        })
-                    }
-                })
+                            priceMessage+=`数据更新时间：${new Date(updateDate).getFullYear()}年${new Date(updateDate).getMonth()+1}月${new Date(updateDate).getDate()}日 ${new Date(updateDate).getHours()}时${new Date(updateDate).getMinutes()}分`;
+                            console.log(priceMessage)
+                            sendGroupMessage(this.port,this.ctx.request.body.group_id,priceMessage)
+                        }
+                    })
+                }
+                else{
+                    errorMessage = `找不到查询的物品，请检查您的物品输入！`;
+                    sendGroupMessage(this.port,this.ctx.request.body.group_id,priceMessage)
+                }
             }
         }
     }
