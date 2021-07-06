ed3eae9aef2734269c099f3af8d0d00e2021140e
NightSiesta <qwer3582919.360354542@gmail.com>
2021-07-06 09:45:31 +0800

    fix the price chinese

--- consumers/priceConsumer.js
+++ consumers/priceConsumer.js
@@ -2,31 +2,6 @@ const Consumer = require("./Consumer");
 const ItemJson = require('../resources/item.js');
 const sendGroupMessage = require('../stdFunc/sendGroupMessage');
 const axios = require("axios");
-const worldName = {
-    "HongYuHai":"红玉海",
-    "ShenYiZhiDi":"神意之地",
-    "LaNuoXiYa":"拉诺西亚",
-    "HuanYingQunDao":"幻影群岛",
-    "MengYaChi":"萌芽池",
-    "YuZhouHeYin":"宇宙和音",
-    "WoXianXiRan":"沃仙曦染",
-    "ChenXiWangZuo":"晨曦王座",
-    "BaiYinXiang":"白银乡",
-    "BaiJinHuanXiang":"白金幻象",
-    "ShenQuanHen":"神拳痕",
-    "ChaoFengTing":"潮风亭",
-    "LvRenZhanQiao":"旅人栈桥",
-    "FuXiaoZhiJian":"拂晓之间",
-    "Longchaoshendian":"龙巢神殿",
-    "MengYuBaoJing":"梦羽宝境",
-    "ZiShuiZhanQiao":"紫水栈桥",
-    "YanXia":"延夏",
-    "JingYuZhuangYuan":"静语庄园",
-    "MoDuNa":"摩杜纳",
-    "HaiMaoChaWu":"海猫茶屋",
-    "RouFengHaiWan":"柔风海湾",
-    "HuPoYuan":"琥珀原"
-}
 class priceConsumerClass extends Consumer{
     constructor(ctx,serverName){
         super(ctx,serverName);
@@ -81,7 +56,7 @@ class priceConsumerClass extends Consumer{
                             }
                             if(filterList.length==0){priceMessage = "未查询到相关商品信息，若查询的内容为HQ，可以尝试查询NQ\n";}
                             for(let i = 0; i<filterList.length; i++){
-                                priceMessage+=(`${filterList[i].pricePerUnit}*${filterList[i].quantity} ${(filterList[i].hq?"HQ":'')} 服务器：${worldName[filterList[i].worldName]}\n`)
+                                priceMessage+=(`${filterList[i].pricePerUnit}*${filterList[i].quantity} ${(filterList[i].hq?"HQ":'')} 服务器：${filterList[i].worldName}\n`)
                             }
                             priceMessage+=`数据更新时间：${new Date(updateDate).getFullYear()}年${new Date(updateDate).getMonth()+1}月${new Date(updateDate).getDate()}日 ${new Date(updateDate).getHours()}时${new Date(updateDate).getMinutes()}分`;
                             console.log(priceMessage)
