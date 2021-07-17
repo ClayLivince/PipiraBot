adb6380739b6ac91383d90f97179486b9e798131
NightSiesta <qwer3582919.360354542@gmail.com>
2021-07-17 20:30:48 +0800

    update legend and alarm

--- timeConsumer/fishAlarm.js
+++ timeConsumer/fishAlarm.js
@@ -2,7 +2,7 @@
 const fishCauculation = require('../stdFunc/fishCauculation');
 var fishAlarm = function(){
     let messages = [];
-    let nameList = ["书记官杀手","水天一碧","暮辉鱼","晨曦旗鱼","莫古尔古球球","千鳍","万事通鲈","红龙","沙里贝涅","云海蝴蝶螺","雕塑家","众神之爱","苦尔鳗","猴面雀杀手","兰代勒翼龙","镰甲鱼","鳍人之敌","深泳的古书","月光虹鳉","珍珠皮皮拉鱼","元首的军扇","战盾剑齿龙鳖"];
+    let nameList = ["红龙", "万事通鲈", "暮辉鱼", "众神之爱", "莫古尔古球球", "云海蝴蝶螺", "雕塑家", "沙里贝涅", "元首的军扇", "能言者", "车轴鱼", "阔面鱼", "书记官杀手", "大祭司鱼", "水天一碧", "头领薄饼章鱼" ,"深咏的古书", "鳍人之敌", "猴面雀杀手", "千鳍", "雷鸣蝾螈",   "兰代勒翼龙"];
     nameList.forEach((name)=>{
         var results = fishCauculation(name).time;
         if(results.realBeginTimes[0]-Date.now()>600000 && results.realBeginTimes[0]-Date.now()<660000){
