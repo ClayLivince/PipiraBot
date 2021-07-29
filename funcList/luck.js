e115333d6a0aab3299f311a7dae13f765fc1099e
NightSiesta <qwer3582919.360354542@gmail.com>
2021-07-29 10:58:38 +0800

    fix sendGroupMessage

--- funcList/luck.js
+++ funcList/luck.js
@@ -6,6 +6,5 @@ var luck = function(info){
     let result = createLuckInformation(id);
     message[0] = result.messages;
     sendGroupMessage('5701',info.group_id,message[0]); //直接发送消息
-    fs.appendFile('../log/log.txt',JSON.stringify(log)+'\n',()=>{});
 }
 module.exports = luck;
