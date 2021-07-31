6a11e20c51512784407505e74c65636c0e88ce6c
NightSiesta <qwer3582919.360354542@gmail.com>
2021-07-31 17:46:44 +0800

    fix alarm

--- server/server.js
+++ server/server.js
@@ -93,7 +93,7 @@ setInterval(function(){ //定时广播
                             if(nickName[fishName]){fishName = nickName[fishName]}
                             var resultTime = fishCauculation(fishName);
                             if(resultTime && resultTime.time.realBeginTimes[0]-Date.now()>600000 && resultTime.time.realBeginTimes[0]-Date.now()<660000){
-                                message = `${fishName}即将进入cd\n:${results2cdmessages(resultTime,1)}`
+                                message = `${results2cdmessages(resultTime,1)}想钓的朋友们注意啦！`
                                 sendGroupMessage('5701',group_id,message);
                             }
                         })
