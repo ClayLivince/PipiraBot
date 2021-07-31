e61ed6a12180b4d9af7135939118fd7c8c243085
NightSiesta <qwer3582919.360354542@gmail.com>
2021-07-31 10:10:30 +0800

    add welcome and broadcast

--- server/server.js
+++ server/server.js
@@ -51,13 +51,20 @@ axios.post('http://localhost:5701'+'/get_group_list',{},{headers:{'Content-Type'
 })
 
 app.use(ctx =>{ //receive message or group_increase
-    console.log(ctx.request.body);
+    if(ctx.request.body.message_type && ctx.request.body.message_type == "group"){ //**群指令处理方式 //
     var group_id = ctx.request.body.group_id; //get the group_id
     var command = ctx.request.body.message.split(/[ ]+/)[0]; //get the command
     var params = ctx.request.body.message.split(/[ ]+/).splice(1); //get the params
     var user_id = ctx.request.body.user_id;//get the user_id
     var role = ctx.request.body.sender.role; //"owner admin member"
-    checkCommand({group_id,command,params,user_id,role}); //检查鉴权 
+    checkCommand({group_id,command,params,user_id,role}); //检查群聊消息以及相关功能的鉴权 
+    }
+    else if(ctx.request.body.notice_type && ctx.request.body.notice_type == "group_increase"){//**群员增加处理方式 */
+        increaseMessage(ctx.request.body);
+    }
+    else if(ctx.request.body.message_type && ctx.request.body.message_type == "private" && ctx.request.body.user_id == 360354542){
+        broadcast(ctx.request.body) //随便写一下
+    }
 })
 
 //主函数每十分钟查看一次群列表,出现新群就初始化mongo的document
@@ -82,3 +89,21 @@ setInterval(function(){ //定时广播
 },60000);
 
 //server.js中需要持久化一个变量
+function broadcast(info){
+    axios.post('http://localhost:5701'+'/get_group_list',{},{headers:{'Content-Type':'application/json'}}).then((res)=>{
+        res.data.data.forEach((group_msg)=>{
+            var group_id = group_msg;//.group_id;
+            sendGroupMessage("5701",group_id,info.message)
+        })
+    })
+}
+function increaseMessage(info){
+    groupModel.find({'groupId':info.group_id},(err,docs)=>{
+        if(docs.length==0){
+            sendGroupMessage('5701',info.group_id,"皮皮拉鱼正在蓄力！还无法发送欢迎消息哦！")
+        }
+        else{
+            sendGroupMessage('5701',info.group_id,`[CQ:at,qq=${info.user_id}],${docs[0].welcomeMsg}`)
+        }
+    })
+}
