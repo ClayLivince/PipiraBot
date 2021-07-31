30907365679330dc608ca8d4b371ce4672765fa4
NightSiesta <qwer3582919.360354542@gmail.com>
2021-07-31 09:09:55 +0800

    fix random

--- funcList/random.js
+++ funcList/random.js
@@ -3,7 +3,7 @@ var random = function (info) {
     let message = [];
     var randomIndex = Math.floor((Math.random() * (info.params.length)));
     if (info.params[randomIndex]) {
-        message[0] = `你的脑海里传来了一个模糊的声音:我是皮皮拉鱼神，帮助纠结的[CQ:at,qq=${info.user_id}]做出选择——${dealtMessage[randomIndex]}`
+        message[0] = `你的脑海里传来了一个模糊的声音:我是皮皮拉鱼神，帮助纠结的[CQ:at,qq=${info.user_id}]做出选择——${info.params[randomIndex]}`
     }
     sendGroupMessage('5701', info.group_id, message[0]); //直接发送消息
 }
