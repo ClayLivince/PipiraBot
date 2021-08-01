e8e0500817b274d796a95762bc1d0070d2887cf3
NightSiesta <qwer3582919.360354542@gmail.com>
2021-08-01 09:26:46 +0800

    fix commonQues

--- funcList/commonQues.js
+++ funcList/commonQues.js
@@ -1,4 +1,5 @@
 const sendGroupMessage = require('../stdFunc/sendGroupMessage');
+const commonQuesJson = require('../resources/commonQues');
 var commonQues = function(info){
     let message = [];
     var params = info.params;
@@ -6,8 +7,8 @@ var commonQues = function(info){
         message[0] = `[CQ:image,file=https://z3.ax1x.com/2021/05/09/gYcWHe.png]`;
     }
     else{
-        if(commonQues[params[0]]){
-                message[0] = `[CQ:at,qq=${ctx.request.body.user_id}] ${commonQues[params[0]]}`;
+        if(commonQuesJson[params[0]]){
+                message[0] = `[CQ:at,qq=${ctx.request.body.user_id}] ${commonQuesJson[params[0]]}`;
         }
         else{
             message[0] = "未找到相关常见问题，请@红豆年糕或者请寻求群友的帮助";
