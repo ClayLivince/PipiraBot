bbd98725f35868d5b5f4642b0ed86eb9e5c934ea
NightSiesta <qwer3582919.360354542@gmail.com>
2021-08-02 21:04:41 +0800

    add sizefamous

--- funcList/famous.js
+++ funcList/famous.js
@@ -1,8 +1,9 @@
     const famousData = require('../resources/famousData');
+    const sizeFamous = require('../resources/sizeFamous');
     const nickName = require('../resources/nickName');
     const sendGroupMessage = require('../stdFunc/sendGroupMessage');
     var famous = function(info){
-        let message = [];
+        let message = [null,null];
         let name = info.params[0]
         if(nickName[name]){name = nickName[name]}
         if(famousData[name]){
@@ -11,6 +12,10 @@
         else{
             message[0] = "目前传说中无此词条，投稿请私聊梦洄子时";
         }
+        if(sizeFamous[name]){
+            message[1] = sizeFamous[name];
+        }
         sendGroupMessage('5701',info.group_id,message[0]); //直接发送消息
+        sendGroupMessage('5701',info.group_id,message[1]);
     }
     module.exports = famous;
